7c5a9c6946fec5c1d22bb2d6976b047c21e1eb42
ruither.borba <delki8@gmail.com>
2023-08-22 21:16:31 -0300

    improving idempotency for financeiro consumer

--- src/main/java/kafka/NewRelatorioFinanceiroConsumer.java
+++ src/main/java/kafka/NewRelatorioFinanceiroConsumer.java
@@ -10,14 +10,19 @@ import org.apache.kafka.common.serialization.IntegerDeserializer;
 import services.NotificationService;
 import java.time.format.DateTimeFormatter;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Map;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class NewRelatorioFinanceiroConsumer extends Thread {
 
   private static final String APP_ID = System.getenv("ONE_SIGNAL_APP_ID");
   private final static String TOPIC = "novo-relatorio-financeiro";
   private final static String INTERNAL_KAFKA_LISTENER = "localhost:29092";
+
+  private Map<LocalDate, String> notifications = new ConcurrentHashMap<>();
 
   @Override
   public void run() {
@@ -32,18 +37,25 @@ public class NewRelatorioFinanceiroConsumer extends Thread {
 
       while (true) {
         ConsumerRecords<String, RelatorioFinanceiro> records = consumer.poll(Duration.ofMillis(1000));
+
         for (ConsumerRecord<String, RelatorioFinanceiro> record : records) {
           String key = record.key();
           RelatorioFinanceiro value = record.value();
           DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yyyy");
           String strJsonBody = "{"
-              + "\"app_id\":\"" + APP_ID + "\","
-              + "\"included_segments\": [\"Subscribed Users\"],"
-              + "\"headings\": {\"en\": \"Atualização Financeira\"},"
-              + "\"contents\": {\"en\": \"área de finanças atualizada com balanço de " + formatter.format(value.getAnoMes()) + "\"},"
-              + "\"web_url\": \"https://cappella.meteorapp.com/\"}";
-          new NotificationService().sendPush(strJsonBody);
+            + "\"app_id\":\"" + APP_ID + "\","
+            + "\"included_segments\": [\"Subscribed Users\"],"
+            + "\"headings\": {\"en\": \"Atualização Financeira\"},"
+            + "\"contents\": {\"en\": \"área de finanças atualizada com balanço de " + formatter.format(value.getAnoMes()) + "\"},"
+            + "\"web_url\": \"https://cappella.meteorapp.com/\"}";
+
+          String notification = notifications.get(LocalDate.now());
+          if (notification == null) {
+            new NotificationService().sendPush(strJsonBody);
+            notifications.put(LocalDate.now(), strJsonBody);
+          }
         }
+
       }
     }
   }
